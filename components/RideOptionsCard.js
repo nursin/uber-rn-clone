e71b96954f50d6694a0699b954757834742be82b
Bobby Keel <bobbykeel76@gmail.com>
2021-09-25 07:00:03 -0500

    bug fixes

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,8 +1,10 @@
 import { useNavigation } from '@react-navigation/core';
 import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
+import { selectTravelTimeInformation, setTravelTimeInformation } from '../redux/slices/navSlice'
 
 const data = [
     {
@@ -25,17 +27,20 @@ const data = [
     },
 ];
 
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
     const navigation = useNavigation();
     const [selected, setSelected] = useState(null);
+    const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
     return (
         <SafeAreaView style={tw`bg-white flex-grow`}>
             <View>
-                <TouchableOpacity onPress={() => navigation.navigate("NavigateCard")} style={tw`absolute top-3 left-5 p-3 rounded-full`}>
+                <TouchableOpacity onPress={() => navigation.navigate("NavigateCard")} style={tw`absolute top-1 left-5 p-3 rounded-full`}>
                     <Icon name="chevron-left" type="fontawesome" />
                 </TouchableOpacity>
-                <Text style={tw`text-center py-5 text-xl`}>Select a ride</Text>
+                <Text style={tw`text-center py-3 text-xl`}>Select a ride - {travelTimeInformation?.distance?.text}</Text>
             </View>
             <FlatList
                 data={data}
@@ -43,7 +48,7 @@ const RideOptionsCard = () => {
                 renderItem={({ item: { id, title, multiplier, image }, item }) => (
                     <TouchableOpacity
                         onPress={() => setSelected(item)}
-                        style={tw`flex-row items-center justify-between px-10 ${id === selected?.id && "bg-gray-200"}`}
+                        style={tw`flex-row items-center justify-between px-4 ${id === selected?.id && "bg-gray-200"}`}
                     >
                         <Image
                             style={{
@@ -53,14 +58,26 @@ const RideOptionsCard = () => {
                             }}
                             source={{ uri: image }}
                         />
-                        <View style={tw`-ml-6`}>
+                        <View style={tw`-ml-4`}>
                             <Text style={tw`text-xl font-bold`}>{title}</Text>
-                            <Text>Travel Time ...</Text>
+                            <Text>{travelTimeInformation?.duration?.text} Travel Time</Text>
                         </View>
-                        <Text style={tw`text-xl`}>$99</Text>
+                        <Text style={tw`text-xl`}>
+                            {new Intl.NumberFormat('en-gb', {
+                                style: 'currency',
+                                currency: 'GBP',
+                            }).format(
+                                (travelTimeInformation?.duration?.value * SURGE_CHARGE_RATE * multiplier) / 100
+                            )}
+                        </Text>
                     </TouchableOpacity>
                 )}
             />
+            <View>
+                <TouchableOpacity disabled={!selected} style={tw`bg-black py-2 mb-3 mx-3 ${!selected && "bg-gray-300"}`}>
+                    <Text style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'tailwind-react-native-classnames';
 import Map from '../components/Map';
@@ -7,13 +7,21 @@ import MapView from 'react-native-maps';
 import NavigateCard from '../components/NavigateCard';
 import { createStackNavigator } from '@react-navigation/stack';
 import RideOptionsCard from '../components/RideOptionsCard';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/core';
 
 const MapScreen = () => {
     const Stack = createStackNavigator();
+    const navigation = useNavigation();
 
     return (
         <View style={tw`bg-white-900 h-full`}>
-
+            <TouchableOpacity
+                onPress={() => navigation.navigate('HomeScreen')}
+                style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+            >
+                <Icon name="menu" />
+            </TouchableOpacity>
             <View style={tw`h-1/2`}>
                 <Map />
             </View>
